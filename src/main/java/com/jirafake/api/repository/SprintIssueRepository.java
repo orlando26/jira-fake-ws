019c3ecacda3408d881ca108706d35b20182da13
lu0 <lucero.alvaradorz@gmail.com>
2021-04-08 01:54:13 -0500

    SprintIssue entity, repository, dto

---
+++ src/main/java/com/jirafake/api/repository/SprintIssueRepository.java
@@ -0,0 +1,9 @@
+package com.jirafake.api.repository;
+
+import com.jirafake.api.entity.SprintIssue;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SprintIssueRepository extends JpaRepository<SprintIssue, Integer>{
+
+}
