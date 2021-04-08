7407d79b4764a6036c2780eff3d7fb65d784de8f
Orlando Loza Castro <orlandolozaarr@gmail.com>
2021-04-08 02:37:11 -0500

    Revert "Lu0/models"

--- src/main/java/com/jirafake/api/repository/IssueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jirafake.api.repository;
-
-import com.jirafake.api.entity.Issue;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IssueRepository extends JpaRepository<Issue, Integer>{
-
-}
